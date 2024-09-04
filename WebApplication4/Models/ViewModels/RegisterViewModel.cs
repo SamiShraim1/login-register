38b5816995e2e626e871cec06ecb05fffb6adb60
SamiShraim1 <samialnser@gmail.com>
2024-09-05 02:22:48 +0300

    Add project files.

---
+++ WebApplication4/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [MaxLength(20)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        [MaxLength(40)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
